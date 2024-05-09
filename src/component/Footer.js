dae65df83baa00c25cb0e113f4353caeb0ddb216
sweta katrodiya <ceswetakatrodiya@gmail.com>
2024-05-10 05:06:19 +0530

    footer

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -5,19 +5,18 @@ function Footer() {
     return (
         <footer className="footer">
             <div className="container">
-                <div className="column ft">
-                    <div className='mainft'>
-                        <div className="col-md-3 fns">
+                <div className="infoabout">
+                    <div className='maininfoabout'>
+                        <div className="fns">
                             <h3>Who we are</h3>
                             <ul>
                                 <li>About us</li>
-                                <li>AbContact us</li>
+                                <li>Contact us</li>
                                 <li>Printing Blog</li>
                                 <li>Careers</li>
-
                             </ul>
                         </div>
-                        <div className="col-md-3 fns">
+                        <div className="fns">
                             <h3>Work with us</h3>
                             <ul>
                                 <li>Corporate account</li>
@@ -26,17 +25,16 @@ function Footer() {
                                 <li>Attiliate program</li>
                             </ul>
                         </div>
-                        <div className="col-md-3 fns">
+                        <div className="fns">
                             <h3>Policies</h3>
                             <ul>
                                 <li>Cancellations, Relums & Refunds</li>
                                 <li>Terms & Conditions</li>
                                 <li>Privacy Policy</li>
-
                             </ul>
                         </div>
 
-                        <div className="col-md-3 fns">
+                        <div className="fns">
                             <h3>Help</h3>
                             <ul>
                                 <li>Order</li>
@@ -44,41 +42,61 @@ function Footer() {
                                 <li>Payment</li>
                                 <li>Shipping</li>
                                 <li>Cancellation & Return</li>
-
                             </ul>
                         </div>
                     </div>
+                </div>
 
+                <div className='lin'></div>
+            </div>
+
+            <div className='container1'>
+                <div className='conftr'>
+                    <div className="blew">
+                        <p className='mbsl'>Marketing Material</p>
+                        <p className='pera'>Flyer Brochure | Tea Coaster | Calendar | Dangler | Standee | Flag | PoP Display | Canopy | Gazebo | Promo Table | Gift Coupon</p>
+                    </div>
+                </div>
+
+                <div className='conftr'>
+                    <div className="blew">
+                        <p className='mbsl'>Box & Packaging</p>
+                        <p className='pera'>Duplex Box | Rigid Box | Corrugated Box | Eco Friendly Box | Paper Sleeve Bag</p>
+                    </div>
+                </div>
+
+                <div className='conftr'>
+                    <div className="blew">
+                        <p className='mbsl'>Signages & Banner</p>
+                        <p className='pera'>Flex Banner | Vinyl Sticker | Acrylic Signage | Safety Signage | Indication Signage | Elevation Signage | Floor Signage | Parking Signage | Basement Signage | Office Signage | Flag</p>
+                    </div>
+                </div>
+                <div className='conftr'>
+                    <div className="blew">
+                        <p className='mbsl'>Sticker & Label</p>
+                        <p className='pera'>Vinyl & PVC stickers | Clear & Transparent stickers | Paper & Laminated Sticker | Holographic stickers | Transfer stickers | Die-cut stickers | Medium Cutting Sticker | Nickel & EP Label | Aluminium Etching & Printed Label | SS Engraving & Printed Label | Silicone Label</p>
+                    </div>
+                </div>
+                <div className='conftr'>
+                    <div className="blew">
+                        <p className='mbsl'>Stationery </p>
+                        <p className='pera'>office Stationary | School Stationary | School Stationary | School Stationary | School Stationary | Pens | Office stationery | Paper stationery | File Folders | Pencils | School stationery | Highlighters | Notebooks | Office File | Erasers | Punching machine | Stapler | Sticky Notes | Ball Pen | Calculator | Envelope | Scissors | Paper | Paper Clip | Rubber stamp | Writing | Copy Paper | Desk Organiser</p>
                     </div>
 
-                    {/* <div className='belw'>
-                        <div className="">
-                            <h3 className='mbsl'>Material</h3>
-                            <p className='pera'>Flyer Brochure | Tea Coaster | Calendar | Dangler | Standee | Flag | PoP Display | Canopy | Gazebo | Promo Table | Gift Coupon</p>
-                        </div>
+                </div>
+                <div className='conftr'>
+                    <div className="blew">
+                        <p className='mbsl'>Gifting</p>
+                        <p className='pera'>Corporate Gift | Personilise  Gift | Occanational Gift</p>
+                    </div>
+                </div>
+            </div>
 
-                        <div className="">
-                            <h3 className='mbsl'>Box & Packaging</h3>
-                            <p className='pera'>Duplex Box | Rigid Box | Corrugated Box | Eco Friendly Box | Paper Sleeve Bag</p>
-                        </div>
+            <div className='line'></div>
 
-                        <div className="">
-                            <h3 className='mbsl'>Signages & Banner</h3>
-                            <p className='pera'>Flex Banner | Vinyl Sticker | Acrylic Signage | Safety Signage | Indication Signage | Elevation Signage | Floor Signage | Parking Signage | Basement Signage | Office Signage | Flag</p>
-                        </div>
-
-                        <div className="">
-                            <h3 className='mbsl'>Sticker & Label</h3>
-                            <p className='pera'>Vinyl & PVC stickers | Clear & Transparent stickers | Paper & Laminated Sticker | Holographic stickers | Transfer stickers | Die-cut stickers | Medium Cutting Sticker | Nickel & EP Label | Aluminium Etching & Printed Label | SS Engraving & Printed Label | Silicone Label</p>
-                        </div>
-                    </div> */}
-                
-</div>
-                <div className='reserv'><p>&copy;2024 Printages. All Rights Reserved.</p></div>
-
-
-
-        
+            <div className='container reserv'>
+                <p>&copy;2024 Printages. All Rights Reserved.</p>
+            </div>
         </footer>
     );
 }
